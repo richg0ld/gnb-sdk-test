b6cc663a300c3d9a3b1963ba9b5d78f265f1b7fa
KIM JUNGHYUN <richg0ld@op.gg>
2019-03-25 00:09:55 +0900

    프로젝트에 gnb 추가할 루트가 없어도 자동으로 추가하도록 변경

--- src/App.js
+++ src/App.js
@@ -12,3 +12,5 @@ const App = () => {
 		</div>
 	)
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+const $root = document.createElement('div');
+$root.id="OPGG";
+const $div = document.getElementsByTagName('div')[0];
+$div.parentNode.insertBefore($root,$div);
+
 ReactDOM.render(<App />, document.getElementById('OPGG'));
