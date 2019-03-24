bdc0a0293cf993e57225663af85a862a43195d95
KIM JUNGHYUN <richg0ld@op.gg>
2019-03-25 00:47:48 +0900

    외부 함수 실행 테스트

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { useState } from "react";
 
 const App = () => {
 	const [value, setValue] = useState('');
+	const [gnbToProjectValue, setGnbToProjectValue] = useState('');
 	
 	const setWorld = () => setValue('world!');
+	
+	const setGnbToProject = () => {
+		const _value = window._kjh.test() || 'Wow!';
+		setGnbToProjectValue(_value);
+	};
 	
 	return (
 		<div>
 			<h1>Hello {value}</h1>
 			<button onClick={setWorld}>click!</button>
+			<hr/>
+			<div>이미 선언되어있는 함수에 외부 함수 선언시 gnb에 영향 줄 수 있는지 테스트 {gnbToProjectValue}</div>
+			<button onClick={setGnbToProject}>click!</button>
+			<hr/>
 		</div>
 	)
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log(1);
+/**
+ * 아래 코드는(_kjh) 나중에 const로 관리해도 될듯
+ */
+
+window._kjh = {
+	test:() => {}
+};
+
+window.kjh = {
+	setTest: callback => window._kjh.test = callback
+};
+console.log(2);
 
 const $root = document.createElement('div');
 $root.id="OPGG";
 const $div = document.getElementsByTagName('div')[0];
 $div.parentNode.insertBefore($root,$div);
+
+
 
 ReactDOM.render(<App />, document.getElementById('OPGG'));
