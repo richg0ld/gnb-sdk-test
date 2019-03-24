015670085fc7c876b9ace18afac09a5421572771
KIM JUNGHYUN <richg0ld@op.gg>
2019-03-24 23:39:44 +0900

    외부 자바스크립트 파일 불러오는 로직

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
-const App = () => {
+import React, { useState } from "react";
 
+const App = () => {
+	const [value, setValue] = useState('');
+	
+	const setWorld = () => setValue('world!');
+	
+	return (
+		<div>
+			<h1>Hello {value}</h1>
+			<button onClick={setWorld}>click!</button>
+		</div>
+	)
 };
